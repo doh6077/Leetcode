f938213e3a1c7ee30b2c1fbc4f03ebbc97f1372d
Dohee Kim <134092191+doh6077@users.noreply.github.com>
2024-11-14 21:28:56 -0500

    Time: 7 ms (91.05%), Space: 16.7 MB (76.09%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,15 +1,6 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False 
-        dicts1, dicts2 ={}, {}
-        for i in range(len(s)):
-            dicts1[s[i]] = 1 + dicts1.get(s[i],0) # if the key doens't exist, it returns zero 
-            dicts2[t[i]] = 1 + dicts2.get(t[i],0) # if the key doens't exist, it returns zero             
-        for c in dicts1:
-            if dicts1[c] != dicts2.get(c,0):
-                return False 
-        return True 
+        return Counter(s) == Counter(t)
           
 
 
