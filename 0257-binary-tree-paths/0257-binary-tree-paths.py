c2ea7f39ec148a1aff9803e4042597e16274b3a5
Dohee Kim <134092191+doh6077@users.noreply.github.com>
2025-05-27 09:15:29 -0400

    Time: 1 ms (21.5%), Space: 12.6 MB (6.94%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.py
+++ 0257-binary-tree-paths/0257-binary-tree-paths.py
@@ -1,23 +1,30 @@
 # Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-class Solution:
-    def binaryTreePaths(self, root: TreeNode) -> List[str]:
-        def dfs(node, path, result):
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def binaryTreePaths(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: List[str]
+        """
+        ans = []
+        def dfs(node, path):
             if not node:
-                return
-
+                return 
             path += str(node.val)
-
-            if not node.left and not node.right:
-                result.append(path)
+            if not node.right and not node.left:
+                ans.append(path)
             else:
-                dfs(node.left, path + '->', result)
-                dfs(node.right, path + '->', result)
-
-        ans = [] 
-        dfs(root, "", ans)
+                dfs(node.right, path + "->")
+                dfs(node.left, path + "->" )
+        
+        dfs(root, "")
         return ans
+        
+
+
+
+        
