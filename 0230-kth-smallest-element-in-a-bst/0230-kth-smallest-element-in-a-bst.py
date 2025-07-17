65edd85b903b888d9527fe933ba10997156c9227
Dohee Kim <134092191+doh6077@users.noreply.github.com>
2025-07-17 11:07:34 -0400

    Time: 0 ms (100%), Space: 21.1 MB (72.38%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -8,30 +8,20 @@
 
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        # This will hold the answer when found
-        self.k = k
-        self.result = None
-        
-        # In-order traversal
-        def inorder(node):
-            if not node:
-                return
-            
-            # Visit the left child first
-            inorder(node.left)
-            
-            # Now visit the node itself
-            self.k -= 1  # Decrease k for each node visited
-            
-            if self.k == 0:  # If we've found the kth smallest, store it
-                self.result = node.val
-                return
-            
-            # If not found yet, visit the right child
-            inorder(node.right)
-        
-        # Start the in-order traversal
-        inorder(root)
-        
-        # Return the result once we've found the kth smallest
-        return self.result
+        # stack iterative 
+        # the one we just popped 
+        # In Order 
+
+        n = 0 
+        stack = [] 
+        cur = root 
+
+        while cur or stack: 
+            while cur:
+                stack.append(cur)
+                cur = cur.left 
+            cur = stack.pop()
+            n += 1 
+            if n == k:
+                return cur.val 
+            cur = cur.right 
