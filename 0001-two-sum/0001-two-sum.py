26a728360ef3ed8530a87c38683e6e334df1ad9f
Dohee Kim <134092191+doh6077@users.noreply.github.com>
2025-08-05 16:08:37 -0400

    Time: 2270 ms (6.62%), Space: 18.6 MB (64.65%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,31 +1,12 @@
+
 class Solution:
-    def twoSum(self, nums: list[int], target: int) -> list[int]:
-        sortednums = sorted(nums)
-        l, r = 0, len(sortednums) -1
-        answers = []
-        while l < r:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # Two Pointers 
+        for i, num1 in enumerate(nums):
+            rest = target - num1
+            for j, num2 in enumerate(nums):
+                if num2 == rest and i != j:
+                    print(f"{i} and {j} and {rest}")
+                    return [i, j]
 
-            if sortednums[r] + sortednums[l] > target:
-                r -= 1
-            elif sortednums[r] + sortednums[l] < target:
-                l += 1
-            else:
-                answers.append(nums.index(sortednums[r]))
-
-                if sortednums[r] == sortednums[l]:
-                    answers.append(nums.index(sortednums[l], nums.index(sortednums[r]) + 1,len(nums)))
-                else:
-                    answers.append(nums.index(sortednums[l]))
-                
-                break
-        return answers
-
-                
-
-
-
-
-
-
-
-
+        
