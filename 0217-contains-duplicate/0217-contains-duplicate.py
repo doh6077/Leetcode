abd2c5a4aa35490da844b4abfc8bf42a1055329a
Dohee Kim <134092191+doh6077@users.noreply.github.com>
2025-11-14 11:46:28 -0500

    Time: 15 ms (54.61%), Space: 31.7 MB (29.35%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,13 +1,17 @@
-class Solution:
-    def containsDuplicate(self, nums: list[int]) -> bool:
-        hashset = set()
+# set에 저장하면서 중복 여부 확인하기
 
-        for i in nums:
-            if i in hashset:
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        seen = set()
+        for v in nums:
+            if v in seen:
                 return True
-            hashset.add(i)
+            seen.add(v)
         return False
-        
-            
+
+
+
+
+
 
         
