740813174ffa61f25a4d60570ce3a3708ee05249
Dohee Kim <134092191+doh6077@users.noreply.github.com>
2025-11-17 19:22:27 -0500

    Time: 4383 ms (5.89%), Space: 17.8 MB (86.01%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,6 +1,27 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
+#        return sorted(s) == sorted(t)
+        if len(s) != len(t):
+            return False
+
+        ptr1, ptr2 = 0, 0 
+        result = {}
+        for i, v in enumerate(s):
+            result[v] = s.count(v)
+        for j, v2 in enumerate(t):
+            
+            if v2 not in result:
+            # this means t has a character that s doesn't have
+                return False
+            else:
+                count_res  = t.count(v2)
+                if count_res != result[v2]:
+                    return False
+        return True 
+
+
+
+
           
 
 
