e7660166a675e970036b3e8fb1e36b54bf066c79
Dohee Kim <134092191+doh6077@users.noreply.github.com>
2025-11-21 16:51:16 -0500

    Time: 2 ms (39.37%), Space: 19.8 MB (30.19%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -1,33 +1,21 @@
-# Definition for a binary tree node.
+from typing import Optional
+
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
 
-
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        # basically we have to check if the left node is less than the root node and the right node is greater than the root node 
-        # DFS, recursive 
-
-        # Base Case - when the node is empty 
-
-        def valid(node, left, right):
+        def validate(node: Optional[TreeNode], low: float, high: float) -> bool:
             if not node:
                 return True
-            if not(node.val > left and node.val < right):
-                return False 
-            return (valid(node.left, left, node.val) and valid(node.right, node.val, right))
-        
-        return valid(root, float("-inf"), float("inf"))
-            
 
+            if not (low < node.val < high):
+                return False
 
+            return (validate(node.left, low, node.val) and
+                    validate(node.right, node.val, high))
 
-
-                
-                    
-
-
-        
+        return validate(root, float("-inf"), float("inf"))
