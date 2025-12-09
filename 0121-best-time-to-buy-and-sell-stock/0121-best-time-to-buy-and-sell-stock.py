837b3d6ce58ac5a4f83e1f13aa577e4fc3842033
Dohee Kim <134092191+doh6077@users.noreply.github.com>
2025-12-09 10:19:41 -0500

    Time: 108 ms (40.54%), Space: 26.8 MB (94.54%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,16 +1,54 @@
 class Solution:
     def maxProfit(self, prices: list[int]) -> int:
-        l, r = 0, 1 
+        left = 0 
+        right = 1
+        profit = 0
+        max_profit = 0 
 
-        maxProfit = 0 
-        while r < len(prices):
-            if prices[l] < prices[r]:
-                profit = prices[r] - prices[l]
-                if maxProfit < profit:
-                    maxProfit = profit
-
+        while right < len(prices):
+            if prices[right] > prices[left]:
+                profit = prices[right] - prices[left]
+                max_profit = max(profit, max_profit)
             else:
-                l = r 
-            r += 1 
-        return maxProfit 
-            
+                left = right
+            right += 1
+        return max_profit
+
+
+
+
+
+
+
+
+        # Two pointesr 
+        # maxProfit = 0 
+        # profit = 0
+        # left, right = 0, len(prices) -1
+        # if len(prices) <3 and prices[right] > prices[left]:
+        #     return prices[right] - prices[left]
+        # for i, v in enumerate(prices):
+        #     value = prices[right] - prices[left] 
+        #     if v < prices[left] and right > i and value > profit:
+        #         left = i
+        #     elif v > prices[right] and left < i and value > profit:
+        #         right = i 
+        #     profit = max(profit, value)
+        #     print(left, right)
+        # return profit 
+        # while left < right: 
+        #     val = prices[right] - prices[left]
+        #     if val > 0: 
+        #         result = max(result, val)
+        #     if prices[left+1] < prices[left]:
+        #         left += 1
+        #     elif prices[right-1] > prices[right]:
+        #         right -= 1
+        #     else:
+        #         return result 
+        # return result 
+        # Time Limit Exceeded
+        # for index, value in enumerate(prices):
+        #     right = prices[index:]
+        #     profit = max(profit, max(right) - value)
+        # return profit
