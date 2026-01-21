7ffb062c18a5dbdc58b59fa954f2d6a59fe5b618
Dohee Kim <134092191+doh6077@users.noreply.github.com>
2026-01-21 13:15:25 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,23 +1,33 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        # Use two pointers, always compare two values in the boundary 
-        result = []
-        intervals = sorted(intervals)
-        left, right = 0, 1 
-        n = len(intervals)
-        while left < right and right < n:
-            if intervals[left][1] >= intervals[right][0] and intervals[left][1] >= intervals[right][1]:
-                del intervals[right]
-                n = len(intervals)
-            elif intervals[left][1] >= intervals[right][0] and intervals[left][1] < intervals[right][1]:
-                intervals[left] = [intervals[left][0], intervals[right][1]]
-                
-                del intervals[right]
-                n = len(intervals)
+        # # Use two pointers, always compare two values in the boundary 
+        # intervals = sorted(intervals)
+        # left, right = 0, 1 
+        # n = len(intervals)
+        # while left < right and right < n:
+        #     if intervals[left][1] >= intervals[right][0] and intervals[left][1] >= intervals[right][1]:
+        #         del intervals[right]
+        #         n = len(intervals)
+        #     elif intervals[left][1] >= intervals[right][0]:
+        #         intervals[left] = [intervals[left][0], intervals[right][1]]        
+        #         del intervals[right]
+        #         n = len(intervals)
+        #     else:
+        #         left += 1 
+        #         right += 1
+        # return intervals 
+
+        
+        intervals.sort(key = lambda interval: interval[0])
+        merged = []
+
+        for interval in intervals:
+            if not merged or merged[-1][1] < interval[0]:
+                merged.append(interval)
             else:
-                left += 1 
-                right += 1
-        return intervals 
+                # there is a overlap
+                merged[-1] = [merged[-1][0], max(merged[-1][1], interval[1])]
+            return merged 
 
 
 
