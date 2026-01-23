105609939cedac2fbc01e47011cc49cc8bc7d4f0
Dohee Kim <134092191+doh6077@users.noreply.github.com>
2026-01-23 12:11:12 -0500

    Time: 13 ms (56.25%), Space: 23.8 MB (27.07%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
@@ -8,46 +8,39 @@ from collections import deque
 class Solution:
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
 
-        # # BFS 
-        # q = [] 
-
-        # self.max_sum = float('-inf')
+        # # Initially thought I need to use BFS ( misunderstood the question)
+        # if not root:
+        #     return 
+        # queue = deque([root])
+        # result = []
         # val = 0
-        # def levelOrder(root,level, val):
-        #     if root is None:  
-        #         val = 0
-        #         return 
-        #     print(root.val)
-        #     val += root.val
-        #     self.max_sum = max(self.max_sum, val)
-        #     print('this is sum', self.max_sum)
-        #     levelOrder(root.left, level + 1, val)
-        #     levelOrder(root.right, level + 1, val)
-            
-            
-        # levelOrder(root, 0, val)
-        # return self.max_sum
+        # max_sum = float('-inf') 
+        # while queue:
+        #     currentNode = queue.popleft()
+        #     result.append(currentNode.val)
+        #     val += currentNode.val
 
-        # left -> root -> right 
+        #     if currentNode.left:
+        #         queue.append(currentNode.left)
+        #         val += currentNode.left.val
+        #     if currentNode.right:
+        #         queue.append(currentNode.right)
+        #         val += currentNode.right.val
+        #     max_sum = max(val, max_sum)
+        #     val = 0
+        # return max_sum
+        res = [root.val]
 
-        if not root:
-            return 
-        queue = deque([root])
-        result = []
-        val = 0
-        max_sum = float('-inf') 
-        while queue:
-            currentNode = queue.popleft()
-            result.append(currentNode.val)
-            val += currentNode.val
-
-            if currentNode.left:
-                queue.append(currentNode.left)
-                val += currentNode.left.val
-            if currentNode.right:
-                queue.append(currentNode.right)
-                val += currentNode.right.val
-            max_sum = max(val, max_sum)
-            val = 0
-        return max_sum
+        def dfs(root):
+            if not root:
+                return 0 
+            leftMax = dfs(root.left)
+            rightMax = dfs(root.right)
+            leftMax = max(leftMax, 0)
+            rightMax = max(rightMax, 0)
             
+            res[0] = max(res[0], root.val + leftMax + rightMax)
+
+            return root.val + max(leftMax, rightMax)
+        dfs(root)
+        return res[0]
